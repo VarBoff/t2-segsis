ccff104d68a571aca124c5c888192ed903c583a0
Varboff <vinirechboff@gmail.com>
2024-11-24 15:09:59 -0300

    ajustei a chave e os valores de entrada

--- main.py
+++ main.py
@@ -10,20 +10,10 @@ p = 0xA4D1CBD5C3FD34126765A442EFB99905F8104DD258AC507FD6406CFF14266D31266FEA1E5C
 our_privatekey = 0x8735367fecc56a4bbf4ba1842fd5f3c6050acc64e97e1669dc6a76c8937d93734fe64c584eff1514d29033aefc76cd8c9b0366876bde0d0020e74b286c6a06461eff01872ac1ebee3b90f1bcf18a2b64fc32ae93f351c4910b672e4cc96e3d63d81c8841ab698f9c455f39e6d65e2abc52907104ad9da3824c6a53e8235aab9e
 their_publickey = 0x73B0F92474CE681F0990F666B6691E8837AE99F2D97853BC5449BD1E1E6B425BD3812C4D57B8509D162599D5543863EB2FECEF541591F033E2F5A6122592DB26B8A6B1BEE9C7402B23682EE710242847FF84336FF9505E827D2A264BCA94F0E3B10BD933FC29CD882F5C6230F9F7FF11BD30267D8A962B8B62361DEFB490FC57
 
-# Google mandou transformar em string, mas n sei se ta correto
-# se pa tem q fazer direto com os bytes do numero
-common_key = gmpy2.powmod(their_publickey, our_privatekey, p)
-sha256_hash = hashlib.sha256(str(common_key).encode('utf-8'))
-key = sha256_hash.digest()[:16]
-
-received_message = bytes.fromhex("f14c3e0f30afe2b3d6ca2e5ac9d8cd4eadb5716236ee27ff7895cfca62e502eaaea51a9cc5a549758fe5339de292a84b925d7dcd7250fb50f3bf9e8c886f84f4")
-
-iv = received_message[:16]
-ciphertext = received_message[16:]
-
-print(f"{key=}")
-print(f"{iv=}")
-print(f"{ciphertext=}")
+def calculate_encription_key(publickey, privatekey, p):
+    common_key = gmpy2.powmod(publickey, privatekey, p)
+    common_key_bytes = common_key.to_bytes((common_key.bit_length() + 7) // 8, byteorder='big')
+    return hashlib.sha256(common_key_bytes).digest()[:16]
 
 def pad(data, block_size=16):
     pad_length = block_size - (len(data) % block_size)
@@ -58,4 +48,18 @@ def decrypt_aes_cbc(key, iv, ciphertext):
 
 #     return unpad(plaintext, 16)
 
-print(f'decrypted={decrypt_aes_cbc(key, iv, ciphertext).decode('utf-8')}')
+def main():
+    key = calculate_encription_key(their_publickey, our_privatekey, p)
+
+    received_message = bytes.fromhex("f14c3e0f30afe2b3d6ca2e5ac9d8cd4eadb5716236ee27ff7895cfca62e502eaaea51a9cc5a549758fe5339de292a84b925d7dcd7250fb50f3bf9e8c886f84f4")
+
+    iv = received_message[:16]
+    ciphertext = received_message[16:]
+
+    print(f"{len(key)=}, {key=}")
+    print(f"{len(iv)=}, {iv=}")
+    print(f"{len(ciphertext)=}, {ciphertext=}")
+
+    print(f'decrypted={decrypt_aes_cbc(key, iv, ciphertext).decode()}')
+
+main()
